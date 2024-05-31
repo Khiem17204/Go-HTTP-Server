873b9570296546ce76ac0010bd7f43373d5470a3
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2024-05-31 20:39:30 +0700

    pass stage

--- app/server.go
+++ app/server.go
@@ -22,25 +22,32 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
-	}
-	req_line := strings.Split(string(buffer), "\r\n")[0]
-	user_agent := strings.Split(string(buffer), "\r\n")[2]
-	path := strings.Split(req_line, " ")[1]
-	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(path, "/")[1] == "echo" {
-		message := strings.Split(path, "/")[2]
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
-	} else if strings.Split(path, "/")[1] == "user-agent" {
-		user_agent_val := strings.TrimPrefix(user_agent, "User-Agent: ")
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(user_agent_val), user_agent_val)))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	}
+	// handle concurrent connections
+	go handle_connection(conn)
+}
 
+func handle_connection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		buffer := make([]byte, 1024)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading request: ", err.Error())
+			os.Exit(1)
+		}
+		req_line := strings.Split(string(buffer), "\r\n")[0]
+		user_agent := strings.Split(string(buffer), "\r\n")[2]
+		path := strings.Split(req_line, " ")[1]
+		if path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else if strings.Split(path, "/")[1] == "echo" {
+			message := strings.Split(path, "/")[2]
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+		} else if strings.Split(path, "/")[1] == "user-agent" {
+			user_agent_val := strings.TrimPrefix(user_agent, "User-Agent: ")
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(user_agent_val), user_agent_val)))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+	}
 }
